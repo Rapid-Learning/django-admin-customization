73d7a5fb783a70ba047b49d7d715819ea50c3d0a
jayparmar271 <jayparmar271@gmail.com>
2022-07-01 02:12:44 +0530

    Save form details via ajax

--- customer/admin.py
+++ customer/admin.py
@@ -2,16 +2,11 @@ from django import forms
 from django.contrib import admin
 from .models import Customer
 
-class CustomerForm(forms.ModelForm):
-    confirm_password = forms.CharField(max_length=30)
-    
 class CustomerAdmin(admin.ModelAdmin):
-    form = CustomerForm
-
-    fieldsets = (
-        (None, {
-            'fields': ('first_name', 'last_name', 'password', 'confirm_password', 'birthdate'),
-        }),
-    )
+    class Media:
+        js = (
+            '//code.jquery.com/jquery-3.6.0.min.js',
+            'customer/custom.js',
+        )
 
 admin.site.register(Customer, CustomerAdmin)
